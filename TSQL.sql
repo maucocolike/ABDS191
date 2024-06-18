4e2e43edf6cbc43135619b75d5e595d41a62d274
maucocolike <122042514@upq.edu.mx>
2024-06-17 18:53:35 -0600

    avance IF WHILE BREAK RETURN
    se vio en la clase algunos de las estrucuras de datos

---
+++ TSQL.sql
@@ -0,0 +1,56 @@
+--TSQL
+/*
+--DECLARAR VARIABLE 
+
+declare @idCliente int
+
+--inicializar o asignar valor
+
+set @idCliente = 8
+
+--if
+
+-- IF @idCliente = 8
+-- select * from Clientes where id_cliente = @idCliente
+
+declare @edad int 
+set @idCliente = 9 
+
+IF @idCliente = 9 
+set @edad = 25
+select * from Clientes where id_clientes = @idCliente
+print @edad
+
+	if EXISTS(select * from Clientes where id_clientes = @idCliente)
+	print 'si existe'
+END
+
+ELSE BEGIN 
+	print 'error:'
+	print 'id no autorizado para la consulta'
+	end
+*/
+
+--while 
+
+declare @contador int = 0
+
+while @contador <=10
+begin
+	print @contador
+	set @contador = @contador + 1
+	if @contador = 3
+		break
+
+	print 'hola'
+end
+print 'aqui continua el flujo'
+begin try
+set @contador = 'maucoco'		
+end try
+begin catch
+print ('la variable del contador solo acepta enteros')
+
+end catch
+
+print ('soy otra consulta')
